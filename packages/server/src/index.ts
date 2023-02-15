d4688656021f3d4eea73966bdd224dd8a959c706
Giuseppe La Torre <giuseppelt@live.com>
2023-02-15 02:43:07 +0100

    feat(server): new useRequestHeader hook

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./errors";
 export { useContext, useContextProperty } from "./context";
+export * from "./hooks";
 export * from "./parsers";
 export * from "./responses";
 export * from "./pipeline";

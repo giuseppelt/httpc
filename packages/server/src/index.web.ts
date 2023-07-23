398bac9e7839b1a2e11e4fce80a3e528a194ed95
Giuseppe La Torre <giuseppelt@live.com>
2023-07-23 03:42:20 +0200

    chore(server): package.json add exports with web key

---
+++ packages/server/src/index.web.ts
@@ -0,0 +1,8 @@
+export * from "./errors";
+export { useContext, useContextProperty, runInContext } from "./context";
+export * from "./hooks";
+export * from "./parsers";
+export * from "./responses";
+export * from "./pipeline";
+export * from "./processor";
+export * from "./middlewares";

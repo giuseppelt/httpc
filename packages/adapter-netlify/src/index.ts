7c06a6b7f2282026cd468d68222f0182aec2c7f6
Giuseppe La Torre <giuseppelt@live.com>
2023-08-04 18:07:30 +0200

    ref(adapter-netlify): updated to new fetch based architecture

--- packages/adapter-netlify/src/index.ts
+++ packages/adapter-netlify/src/index.ts
@@ -1,6 +1,4 @@
-import { Readable, Writable } from "stream";
-import type { IncomingMessage, OutgoingHttpHeaders, ServerResponse } from "http";
-import { HttpCServerOptions, HttpCServerRequestProcessor, createHttpCServerProcessor } from "@httpc/server";
+import { HttpCServer, HttpCServerOptions, createHttpCServer } from "@httpc/server";
 import type { Handler, HandlerEvent } from "@netlify/functions";
 
 
@@ -21,115 +19,62 @@ export type HttpCNetlifyAdapterOptions = Pick<HttpCServerOptions,
 }
 
 
-let handler: HttpCServerRequestProcessor | undefined;
-let initialized = false;
-let initializing: Promise<void> | undefined;
+let server: HttpCServer | undefined;
+
+//TODO: add a @httpc/kit solution
 
 export function createHttpCNetlifyHandler(options: HttpCNetlifyAdapterOptions): Handler {
-    if (!handler || options.refresh) {
-        handler = createHttpCServerProcessor(options);
-    }
+    const handler = (!server || options.refresh)
+        ? (server = createHttpCServer(options))
+        : server;
 
     return async (event, context) => {
-        if (options.kit && !initialized) {
-            if (!initializing) {
-                initializing = Promise.resolve().then(async () => {
-                    const lib = "@httpc/kit";
-                    const { initializeContainer } = await import(lib);
-                    await initializeContainer();
-
-                    initialized = true;
-                    initializing = undefined;
-                });
-            }
-
-            await initializing;
-        }
-
         const request = createRequest(event);
-        const response = createResponse();
-
-        await Promise.allSettled([
-            handler!(request, response),
-            new Promise((resolve, reject) => {
-                response.on("close", resolve);
-                response.on("error", reject);
-            })
-        ]);
-
-        const buffer = response.getBuffer();
+        const response = await handler.fetch(request, {
+            requestId: context.awsRequestId
+        });
 
         return {
-            statusCode: response.statusCode,
-            headers: response.getHeaders() as any,
-            body: buffer?.toString("utf8")
+            statusCode: response.status,
+            headers: Object.fromEntries(response.headers.entries()),
+            ...await createResponseBody(response)
         };
     };
 }
 
 
-function createRequest(event: HandlerEvent): IncomingMessage {
-    let body: Readable;
+function createRequest(event: HandlerEvent): Request {
+    const url = event.rawUrl;
+    const method = event.httpMethod;
+    const headers = event.headers as Record<string, string>;
+    const body = event.body && method !== "GET" && method !== "HEAD" && (
+        event.isBase64Encoded ? Buffer.from(event.body, "base64") : event.body
+    ) || undefined;
 
-    if (event.body) {
-        if (typeof event.body === "string") {
-            body = Readable.from(Buffer.from(event.body, event.isBase64Encoded ? "base64" : "utf8"));
-        } else {
-            console.warn("Body (%s): %s", typeof event.body, (event.body as any)?.toString());
-            throw new Error("Unknown body type");
-        }
-    } else {
-        body = Readable.from([]);
-    }
-
-    return Object.assign(body, {
-        url: event.rawUrl,
-        method: event.httpMethod,
-        headers: event.headers,
-    } as IncomingMessage);
+    return new Request(url, {
+        method,
+        headers,
+        body
+    });
 }
 
-function createResponse() {
-    const writable = new BufferedWritable();
 
-    let _statusCode = 200;
-    let _headers: OutgoingHttpHeaders = {};
-
-    return Object.assign(writable, {
-        get statusCode() { return _statusCode },
-        set statusCode(value: number) { _statusCode = value },
-
-        getHeaders() {
-            return _headers;
-        },
-
-        setHeader(name, value) {
-            _headers[name] = value as any;
-        },
-
-        writeHead(statusCode, headers: any) {
-            _statusCode = statusCode;
-            _headers = { ..._headers, ...headers };
-            return this;
-        }
-    } as ServerResponse);
-}
-
-class BufferedWritable extends Writable {
-    protected _buffer?: Buffer | undefined;
-
-    getBuffer() {
-        return this._buffer;
+async function createResponseBody(response: Response) {
+    if (!response.body) {
+        return;
     }
 
-    _write(chunk: any, encoding: BufferEncoding, callback: (error?: Error | null | undefined) => void): void {
-        const newChunk = Buffer.from(chunk, encoding);
-        if (!this._buffer) {
-            this._buffer = chunk;
-        } else {
-            this._buffer = Buffer.concat([this._buffer, newChunk]);
-        }
+    let chunks: Uint8Array[] = [];
+    const reader = response.body.getReader();
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
 
-        callback();
+        chunks.push(value);
+    }
+
+    return {
+        body: Buffer.concat(chunks).toString("base64"),
+        isBase64Encoded: true
     }
 }

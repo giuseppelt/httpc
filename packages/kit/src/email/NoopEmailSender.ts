5c5fb3cd51d36e5bdd3dcdbfadd2316a3a402402
Giuseppe La Torre <giuseppelt@live.com>
2023-09-02 03:34:54 +0200

    fix(kit): missing noop email injection logger

--- packages/kit/src/email/NoopEmailSender.ts
+++ packages/kit/src/email/NoopEmailSender.ts
@@ -1,13 +1,21 @@
 import { singleton } from "tsyringe";
 import { assert } from "../internal";
 import type { IEmailSender, SendEmailParams } from "./types";
+import { type ILogger, logger } from "../logging";
 import { BaseService } from "../services";
 
 
 @singleton()
 export class NoopEmailSender extends BaseService() implements IEmailSender {
+    constructor(
+        @logger() logger: ILogger,
+    ) {
+        //@ts-expect-error
+        super(...arguments);
+    }
+
     async send(options: SendEmailParams) {
-        assert(!!(options.bodyHtml || options.bodyText), "one of bodyHtml and bodyText is required");
+        assert(!!(options.bodyHtml || options.bodyText), "One of bodyHtml and bodyText is required");
 
         if (Array.isArray(options.to)) {
             assert(options.to.length > 0, "options.to must have at least an item");

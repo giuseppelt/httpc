30a992ec0954ca521d4de24df1d034baac484c0a
Giuseppe La Torre <giuseppelt@live.com>
2026-01-22 13:03:12 +0100

    ref(kit): optimized winston logger with a shared root

--- packages/kit/src/logging/winston/WinstonLogService.ts
+++ packages/kit/src/logging/winston/WinstonLogService.ts
@@ -6,14 +6,13 @@ import { WinstonLogger } from "./WinstonLogger";
 
 
 type WinstonProperties = {
-    level?: LogLevel
     [key: string]: any
 }
 
-type FactoryOption<T> = T | ((label: string, properties?: WinstonProperties) => T)
+type FactoryOption<T> = T | ((properties?: WinstonProperties) => T)
 
 export type WinstonLogServiceOptions = {
-    level?: FactoryOption<LogLevel>
+    level?: LogLevel
     properties?: FactoryOption<WinstonProperties>
     transports?: FactoryOption<transport | transport[]>
 }
@@ -21,76 +20,67 @@ export type WinstonLogServiceOptions = {
 @singleton()
 @alias(KEY("ILogService"))
 export class WinstonLogService implements ILogService {
+    protected readonly _rootLogger: winston.Logger;
+
     constructor(
         @options(undefined) protected options?: WinstonLogServiceOptions
     ) {
+        this._rootLogger = this._createRoot(options);
     }
 
     createLogger(label: string, properties?: WinstonProperties): ILogger {
-        let lazyProperties: ((level: string, properties?: WinstonProperties) => any) | undefined;
+        const commonProperties = typeof this.options?.properties === "function"
+            ? this.options!.properties!()
+            : this.options?.properties;
 
-        if (this.options?.properties) {
-            if (typeof this.options.properties === "function") {
-                lazyProperties = this.options.properties as any;
-                properties = { ...lazyProperties!(label, properties), ...properties };
-            } else {
-                properties = { ...this.options.properties, ...properties };
-            }
-        }
 
-        let { level } = properties || {};
-        if (!level && this.options?.level) {
-            if (typeof this.options.level === "string") {
-                level = this.options.level;
-            } else {
-                level = this.options.level(label, properties);
-            }
-        }
+        return new WinstonLogger(this._rootLogger.child({
+            label,
+            ...commonProperties,
+            ...properties
+        }));
+    }
+
+    protected _createRoot(options?: WinstonLogServiceOptions): winston.Logger {
+        let {
+            level,
+            properties,
+            transports,
+        } = options || {};
 
         if (!level) {
             level = process.env.LOG_LEVEL || (process.env.NODE_ENV === "production" ? "info" : "silly") as any;
         }
 
-        let transports = typeof this.options?.transports === "function"
-            ? this.options.transports(label, properties)
-            : this.options?.transports;
+        if (typeof transports === "function") {
+            transports = transports(options?.properties);
+        }
 
         if (!transports) {
             transports = new winston.transports.Console({
                 format: format.combine(
-                    // this._createContext(),
                     process.env.NODE_ENV === "production" ? format.combine() /* noop in production */ : format.colorize(),
-                    this._createConsoleFormat(label, lazyProperties),
+                    this._createConsoleFormat(properties),
                 )
             })
         }
 
-        const logger = winston.createLogger({
+        return winston.createLogger({
             defaultMeta: properties,
             level,
             transports
         });
-
-        return new WinstonLogger(logger);
     }
 
-
-    // protected _createContext() {
-    //     return format((info, opts) => Object.assign(info, opts))({
-    //         environment: process.env.NODE_ENV || "development"
-    //     });
-    // }
-
-    protected _createConsoleFormat(label: string, properties?: (label: string) => any) {
+    protected _createConsoleFormat(properties?: FactoryOption<WinstonProperties>) {
         return format.combine(
             format.timestamp(),
-            format.label({ label }),
             format.splat(),
             format.printf(info => {
                 const { level, timestamp, label, message, ...meta } = info;
 
                 if (properties) {
-                    Object.assign(meta, properties(label), meta);
+                    Object.assign(meta, typeof properties === "function" ? properties() : properties, meta);
                 }
 
                 let extra = JSON.stringify(meta);
